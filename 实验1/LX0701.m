b32b011f7b3af89a7118403080042b17e5c5d892
misachu <railmisachu@outlook.com>
2021-01-15 14:29:36 +0800

    Update 1 3 4 5

---
+++ 实验1/LX0701.m
@@ -0,0 +1,8 @@
+A=[1, 1, 1; 1, 2, 3; 1, 3, 6]; 
+B=[8, 1, 6; 3, 5, 7; 4, 9, 2];
+iand = A+B;
+disp('A+B=')
+disp(iand)
+imin = A-B;
+disp('A-B=')
+disp(imin)
---
+++ 实验1/LX0710.m
@@ -0,0 +1,3 @@
+syms x y;
+p=expand((x+1)^3)
+q=expand(sin(x+y))
